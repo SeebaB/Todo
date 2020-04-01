bb69786e65120050f613b8ab7852ca6ea0f86b63
Sebastian Buczek <sebastian.buczek92@gmail.com>
2020-04-01 14:00:30 +0200

    Convert to MVC design pattern.

---
+++ Todo/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Cell.swift
+//  Todo
+//
+//  Created by Sebastian Buczek on 01/04/2020.
+//  Copyright © 2020 Sebastian Buczek. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+}
